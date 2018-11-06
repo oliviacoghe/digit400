8c92af5ebc34ae94bcaf3d4808b0c61951944435
Olivia Coghe <omc5031@psu.edu>
2018-11-06 04:37:01 +0000

    connected about page and added logo

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ def login_required(f):
     
 # CMS Structure title, path, message
 APP_CONTENT = {
-    "Home":[["Welcome","/welcome/","Welcome to my App, you can do many things here."],["Background","/background/","We had a lot of fun building this app. Learn more about our story"],["Messages","/messages/","Get your messages from the community!"]], 
+    "Home":[["Welcome","/welcome/","Welcome to my App, you can do many things here."],["Background","/about/","We had a lot of fun building this app. Learn more about our story"],["Messages","/messages/","Get your messages from the community!"]], 
     
     "Profile":[["User Profile","/userprofile/","Edit your profile here!"],["Terms of Service", "/tos/","The legal stuff!"],["Photo Upload", "/upload", "Upload your user profile photo here."],],
     
